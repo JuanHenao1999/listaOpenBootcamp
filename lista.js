7fd28786a7b744a17b7fbc3c0d703be868361b58
JuanHenao1999 <juanhenao123kl@gmail.com>
2023-01-02 08:23:26 -0500

    lista
    se crea lista con objetos sobre mis datos y libro favorito

---
+++ lista.js
@@ -0,0 +1,10 @@
+const nombre = "Juan Carlos Henao Hernandez";
+var edad = 23;
+const soyDesarrollador = true;
+const fecha_nacimiento = new Date( "Nov 20 1999");
+
+const libroFavorito = {
+    titulo: "Padre Rico, Padre Pobre",
+    autor: "Robert Kiyosaki",
+    fecha: 2004,
+}
